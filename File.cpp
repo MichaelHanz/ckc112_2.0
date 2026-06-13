7a95dba4788348f55f3baa8fb977ddf40b43bd16
thamyuxiang <yuxiangtham0519@gmail.com>
2026-06-13 22:03:14 +0800

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -1,11 +1,22 @@
 #include "File.h"
 
-using namespace std;
+using namespace std; 
 
-File::File(string name, string extension)
-    : name(name), extension(extension) {}
+// Constructor to initialize file details using optimized memory passing
+File::File(const string& fileName, const string& fileExtension) 
+    : name(fileName), extension(fileExtension) {}
 
-string File::getFullName()
-{
+// Returns the raw string name
+string File::getName() const {
+    return name;
+}
+
+// Returns the raw string extension
+string File::getExtension() const {
+    return extension;
+}
+
+// Combines name and extension with a dot safely inside const operations
+string File::getFullName() const {
     return name + "." + extension;
 }
