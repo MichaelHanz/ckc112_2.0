508fed296b234aa36e089f8b0e70d26e8a96c947
thamyuxiang <yuxiangtham0519@gmail.com>
2026-06-14 11:55:53 +0800

    Update FileSystem.h

--- FileSystem.h
+++ FileSystem.h
@@ -20,19 +20,19 @@ public:
     void loadFile();
     void run();
 
-    // Student 3
+    
     void enterFolder(string foldername);
     void showCurrentPath() const;
     void showCurrentFolder() const;
     void goBack();
 
-    // Student 2
+    
     void createFolder(string foldername);
     void createFile(string filename, string type);
     void deleteFile(string filename);
     void deleteFolder(string foldername);
 
-    // Student 1
+    
     void displayFullTree() const;
     void searchFile(string filename) const;
 };
