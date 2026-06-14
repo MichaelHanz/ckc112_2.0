a864b05fb3c28c86a58fde5a79dec089e0b300b5
thamyuxiang <yuxiangtham0519@gmail.com>
2026-06-14 16:12:17 +0800

    Update FileSystem.h

--- FileSystem.h
+++ FileSystem.h
@@ -10,30 +10,37 @@ class FileSystem
 {
     Folder *root;
     Folder *current;
-    vector<string> splitString(string str, char delimiter); // Helper function to split the root strings
-    Folder *findFolder(vector<string> pathSegments);        // Helper Function for loadFromFile
+    vector<string> splitString(string str, char delimiter);
+    Folder *findFolder(vector<string> pathSegments);
 
 public:
+    //------------------------exception--------------------------------
+    class RootNavigationException {}; 
+    class FolderNavigationException {
+    private:
+        string missingFolder;
+    public:
+        FolderNavigationException(string name) { missingFolder = name; }
+        string getName() const { return missingFolder; }
+    };
+    // =================================================================
+
     FileSystem();
     ~FileSystem();
 
-    // The one time run functions in the "FileSystem" Object
     void loadFile();
     void run();
 
-    // Student 3
     void enterFolder(const string &foldername);
     void showCurrentPath() const;
     void showCurrentFolder() const;
     void goBack();
 
-    // Student 2
     void createFolder(const string &foldername);
     void createFile(const string &filename, const string &type);
     void deleteFile(const string &filename);
     void deleteFolder(const string &foldername);
 
-    // Student 1
     void displayFullTree() const;
     void searchFile(const string &filename) const;
 };
